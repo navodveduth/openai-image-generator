a1130c90c3a30f2fb22bd966f768a14d8d5dd838
Navod <54998118+navodveduth@users.noreply.github.com>
2022-12-12 17:55:31 +0530

    openAI backend

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use('/openai', require('./routes/openaiRoutes'));
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
